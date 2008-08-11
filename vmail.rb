bfb4e018e4dbf304435b639ea14cb69861a07c0d
Scott Jann <sjann@trms.com>
2008-08-11 11:08:01 -0500

    Added integration with SalesForce for contact lookup

--- vmail.rb
+++ vmail.rb
@@ -1,12 +1,99 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
+require 'activerecord'
 require 'net/smtp'
 require 'rmail'
 require 'tlsmail'
 require 'yaml'
 
 config = YAML.load_file(File.dirname(__FILE__) + "/config.yml")
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => "activesalesforce",
+  :url      => config['salesforce_url'],
+  :username => config['salesforce_user'],
+  :password => config['salesforce_password']
+)
+
+class Contact < ActiveRecord::Base
+end
+
+class Account < ActiveRecord::Base
+end
+
+class Case < ActiveRecord::Base
+end
+
+def id_to_url(s)
+  s = s[0,s.length-3]
+  "#{config['salesforce_base_url']}/#{s}"
+end
+
+def lookup_account(p)
+  begin
+    Account.find_by_phone(p)
+  rescue Exception => e
+#    print "Error - #{e.to_s}\n"
+  end
+  a = Account.find_by_phone(p)
+  a = Account.find_by_fax(p) if a.nil?
+  return a
+end
+
+def lookup_contact(p)
+  begin
+    Contact.find_by_phone(p)
+  rescue Exception => e
+#    print "Error - #{e.to_s}\n"
+  end
+  c = Contact.find_by_phone(p)
+  c = Contact.find_by_fax(p) if c.nil?
+  c = Contact.find_by_mobile_phone(p) if c.nil?
+  c = Contact.find_by_home_phone(p) if c.nil?
+  c = Contact.find_by_other_phone(p) if c.nil?
+  c = Contact.find_by_assistant_phone(p) if c.nil?
+  return c
+end
+
+def lookup_phone(p)
+  result = "";
+  begin
+    a = lookup_account(p)
+    c = lookup_contact(p)
+    if a.nil? && c.nil? == false
+      a = Account.find_by_id(c.account_id)
+    end
+
+    if c.nil? == false
+#      print "Contact ID is #{c.id}\n"
+      begin
+        Case.find(:all, :conditions => {:contact_id=>c.id, :is_closed=>false})
+      rescue Exception => e
+#        print "Error - #{e.to_s}\n"
+      end
+      cases = Case.find(:all, :conditions => {:contact_id=>c.id, :is_closed=>false})
+
+      result += "Contact for #{p} is <a href=\"#{id_to_url(c.id)}\">#{c.name}</a> (#{cases.length} open cases)\n"
+    end
+    if a.nil? == false
+#      print "Account ID is #{a.id}\n"
+      begin
+        Case.find(:all, :conditions => {:account_id=>a.id, :is_closed=>false})
+      rescue Exception => e
+#        print "Error - #{e.to_s}\n"
+      end
+      cases = Case.find(:all, :conditions => {:account_id=>a.id, :is_closed=>false})
+
+      result += "Account for #{p} is <a href=\"#{id_to_url(a.id)}\">#{a.name}</a> (#{cases.length} open cases)\n"
+    end
+  rescue Exception => e
+    result += "Can't find #{p} in SalesForce - #{e.to_s}\n"
+  end
+  
+  result = "Can't find #{p} in SalesForce\n" if result.empty?
+  return result
+end
 
 message = RMail::Parser.read(STDIN)
 
@@ -49,7 +136,8 @@ content = RMail::Message::new
 content.header.set('Content-Type', 'text/plain',
         'charset' => 'us-ascii')
 content.header.set('Content-Disposition', 'inline')
-content.body = "You received a voicemail from #{callerID} at #{callDate}"
+content.body = "You received a voicemail from #{callerID} at #{callDate}\n\n"
+content.body += lookup_phone(callerID)
 mail.add_part(content)
 
 part = RMail::Message::new
@@ -63,7 +151,7 @@ File::open(output) do |fh|
 end
 mail.add_part(part)
 
-print "Sending email to #{email}...\n"
+#print "Sending email to #{email}...\n"
 
 if config['use_smtp']
   Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE) if config['use_smtp_ssl']
@@ -77,4 +165,3 @@ else
 end
 
 File.delete(output)
-
